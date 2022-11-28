96753dec415b88d8faa6a1aa0158fe561fe4205f
Begaiym <begoshaaaa@gmail.com>
2022-11-28 13:38:05 +0600

    update func & requirements

--- app.py
+++ app.py
@@ -197,6 +197,7 @@ function_pointers = {
 }
 
 app = Dash(__name__)
+server = app.server
 
 app.layout = html.Div([
 
@@ -235,4 +236,3 @@ def update_graph(option_slctd):
 if __name__ == '__main__':
     app.run_server("0.0.0.0", debug=False, port=int(os.environ.get('PORT', 8000)))
 
-server = app.server
